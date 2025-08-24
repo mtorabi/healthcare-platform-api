18484a7bc21c370e5d7afd360e4ad8eb4f57d469
torabi.mohammad <torabi.mohammad@foodmarkethub.com>
2025-08-24 07:42:18 +0200

    avoid using datasource elsewhere (unexpected token issue)

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { Type } from 'class-transformer';
-import { dataSourceOptions } from './data-source';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot(dataSourceOptions),
-    ],
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+            type: 'postgres',
+            host: config.get<string>('DB_HOST'),
+            port: config.get<number>('DB_PORT'),
+            username: config.get<string>('DB_USER'),
+            password: config.get<string>('DB_PASS'),
+            database: config.get<string>('DB_NAME'),
+            synchronize: true,
+            autoLoadEntities: true,
+      }),
+    })],
     providers: [],
     exports: [],
 })
