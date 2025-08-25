293def9fc794c2cfdc49de43fc14f020eaa1c29a
torabi.mohammad <torabi.mohammad@foodmarkethub.com>
2025-08-25 05:24:52 +0200

    add cross origin policy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { DBExceptionFilter } from './filters/db_execution.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   const config = new DocumentBuilder()
     .setTitle('Healthcare API')
     .setDescription('We provide comprehensive healthcare solutions.')
